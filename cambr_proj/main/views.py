87c60aac1f10409ff202ac5abb7d0db6a840c677
Nicholas Herriot <Nicholas.Herriot@gmail.com>
2017-01-17 11:31:04 +0000

    some changes to remove hard coded template objects that are being used over may templates but created in only certain views.

--- cambr_proj/main/views.py
+++ cambr_proj/main/views.py
@@ -9,17 +9,44 @@ from .forms import HomeForm
 from django.contrib.auth.decorators import login_required, permission_required
 
 
-
 def index_page_view(request):
-    data_obj = Home.objects.all()
-    print data_obj
-    context = {
-        'query_page': data_obj,
-    }
+    """
+    Description:
+        This handles the creation of an index page. The index page takes data from the Home model and renders. In this
+        instance we only ever have 1 item in our table. This first item can be updated but not deleted. If it does not
+        exist the page will contain an 'add' button to allow the user to add the first item to the page to be rendered.
+
+    :param request:
+        This takes a request object. It should contain:
+
+    :return:
+        render
+        home.html template - containing the first Home object if it exists. Otherwise it contains the 'add' button.
+        context - containing the Home object which will populate the template.
+    """
+
+    no_home_page_text = False
+    try:
+        home_main = Home.objects.get(pk=1)
+        print "The title is: ", home_main.title
+        print "The slug is: ", home_main.slug
+        print "The main content is: ", home_main.content
+
+    except Home.DoesNotExist:
+        print "The home page does not exist. Make sure we can render the add button"
+        no_home_page_text = True
+
+    print " This is the index_page view..."
+
+    context = {'query_page': home_main,
+               'no_text': no_home_page_text}
+
     return render(request, 'home.html', context)
+
 
 @login_required
 def create_index_view(request):
+    print "Create index view"
     form = HomeForm(request.POST or None, request.FILES or None)
     if form.is_valid():
         obj_form = form.save(commit=False)
@@ -49,7 +76,7 @@ def detail_index_view(request, slug=None):
         'slug': instance_obj.slug,
         'query_page': title_page
     }
-     
+
     return render(request, 'index_detail.html', context)
 
 
